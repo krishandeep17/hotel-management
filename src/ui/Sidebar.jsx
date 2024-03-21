7d33881c984993f3f870adf035c9ac08c9e1bb2f
Krishandeep Singh <krishandeep17@gmail.com>
2024-03-21 18:28:36 +0530

    feat: add collapse/expand sidebar button

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,24 +1,58 @@
+import { useState } from "react";
 import styled from "styled-components";
 
-import Logo from "./Logo";
-import MainNav from "./MainNav";
+import SidebarHeader from "./SidebarHeader";
+import SidebarNav from "./SidebarNav";
 
 const StyledSidebar = styled.aside`
+  --padding-block: 2.4rem;
+  --padding-inline: 1.6rem;
+
+  grid-area: sidebar;
+
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 20;
+  width: ${(props) => (props.isCollapsed ? "8.08rem" : "26rem")};
+  transition: width 0.4s;
+  height: 100svh;
+
+  @supports not (height: 100svh) {
+    /* Fallback for browsers without svh(small viewport height) support */
+    height: 100vh;
+  }
+
   background-color: var(--color-grey-0);
-  padding: 3.2rem 2.4rem;
+  padding: var(--padding-block) var(--padding-inline);
   border-right: 1px solid var(--color-grey-100);
 
-  grid-row: 1 / -1;
-  display: flex;
-  flex-direction: column;
-  gap: 3.2rem;
+  & button {
+    opacity: 0;
+    pointer-events: none;
+    visibility: hidden;
+    transform: translateX(-5px);
+  }
+
+  &:hover button {
+    opacity: 1;
+    pointer-events: auto;
+    visibility: visible;
+    transform: translateX(0);
+  }
 `;
 
 export default function Sidebar() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
   return (
-    <StyledSidebar>
-      <Logo />
-      <MainNav />
+    <StyledSidebar isCollapsed={isCollapsed}>
+      <SidebarHeader
+        isCollapsed={isCollapsed}
+        setIsCollapsed={setIsCollapsed}
+      />
+
+      <SidebarNav isCollapsed={isCollapsed} />
     </StyledSidebar>
   );
 }
---
+++ src/ui/SidebarHeader.jsx
@@ -0,0 +1,62 @@
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
+import styled from "styled-components";
+
+import lightLogo from "../assets/logo-light.png";
+
+const StyledHeader = styled.header`
+  position: relative;
+  margin-bottom: 3.2rem;
+  min-height: 9.6rem;
+`;
+
+const StyledButton = styled.button`
+  position: absolute;
+  left: 100%;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 2;
+
+  border: none;
+  background-color: var(--color-brand-600);
+  padding: 0.4rem;
+  border-radius: var(--border-radius-sm);
+  transition: 0.2s;
+
+  &:hover {
+    background-color: var(--color-brand-700);
+  }
+
+  & svg {
+    width: 2.4rem;
+    height: 2.4rem;
+    color: var(--color-brand-50);
+  }
+`;
+
+const Img = styled.img`
+  margin-inline: auto;
+  width: 13.5rem;
+`;
+
+export default function SidebarHeader({ isCollapsed, setIsCollapsed }) {
+  return (
+    <StyledHeader>
+      <StyledButton
+        title={isCollapsed ? "Expand Menu" : "Collapse Menu"}
+        onClick={() => setIsCollapsed(!isCollapsed)}
+      >
+        {isCollapsed ? <HiChevronRight /> : <HiChevronLeft />}
+      </StyledButton>
+
+      <picture>
+        <Img
+          src={lightLogo}
+          alt="Logo"
+          width="150"
+          height="107"
+          loading="lazy"
+        />
+      </picture>
+    </StyledHeader>
+  );
+}
