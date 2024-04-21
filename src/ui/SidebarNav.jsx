da2d2c563cbf9546986f8242d24886d1e15265d9
Krishandeep Singh <krishandeep17@gmail.com>
2024-04-21 12:39:11 +0530

    fix: encapsulate NavLink styling within NavItem

--- src/ui/SidebarNav.jsx
+++ src/ui/SidebarNav.jsx
@@ -14,100 +14,102 @@ const NavList = styled.ul`
   gap: 0.8rem;
 `;
 
-const StyledNavLink = styled(NavLink)`
-  &:any-link {
-    display: flex;
-    align-items: center;
-    gap: 1.2rem;
+const NavItem = styled.li`
+  a {
+    &:any-link {
+      display: flex;
+      align-items: center;
+      gap: 1.2rem;
 
-    position: relative;
-    border-radius: var(--border-radius-sm);
-    color: var(--color-grey-600);
-    font-size: 1.6rem;
-    font-weight: 500;
-    padding: 1.2rem;
-    transition: background-color 0.3s, color 0.2s;
+      position: relative;
+      border-radius: var(--border-radius-sm);
+      color: var(--color-grey-600);
+      font-size: 1.6rem;
+      font-weight: 500;
+      padding: 1.2rem;
+      transition: background-color 0.3s, color 0.2s;
+    }
+
+    /* This works because react-router places the active class on the active NavLink */
+    &:hover,
+    &:active,
+    &.active:any-link {
+      background-color: var(--color-brand-50);
+      color: var(--color-brand-600);
+    }
+
+    & svg {
+      height: 2.4rem;
+      width: 2.4rem;
+      min-width: 2.4rem;
+      color: var(--color-grey-600);
+      transition: color 0.2s;
+    }
+
+    &:hover svg,
+    &:active svg,
+    &.active:any-link svg {
+      color: var(--color-brand-600);
+    }
+
+    & span {
+      opacity: 1;
+      pointer-events: auto;
+      visibility: visible;
+      transition: opacity 0.3s;
+    }
+
+    ${(props) =>
+      props.isCollapsed &&
+      css`
+        &::before,
+        &::after {
+          position: absolute;
+          top: 50%;
+
+          opacity: 0;
+          pointer-events: none;
+          visibility: hidden;
+          transform: translate(-5px, -50%);
+          transition: 0.2s;
+        }
+
+        &::before {
+          content: attr(data-tooltip);
+          background-color: var(--color-grey-800);
+          color: var(--color-brand-50);
+          border-radius: var(--border-radius-sm);
+          font-weight: 500;
+          font-size: 1.4rem;
+          padding: 0.44rem 0.8rem;
+          left: calc(100% + var(--padding-inline) + 5px);
+        }
+
+        &::after {
+          content: "";
+          border: 5px solid transparent;
+          border-right-color: var(--color-grey-800);
+          border-left: none;
+          left: calc(100% + var(--padding-inline) + 1px);
+        }
+
+        &:hover::before,
+        &:hover::after,
+        &:focus::before,
+        &:focus::after {
+          opacity: 1;
+          pointer-events: auto;
+          visibility: visible;
+          transform: translate(0, -50%);
+        }
+
+        & span {
+          opacity: 0;
+          pointer-events: none;
+          visibility: hidden;
+        }
+      `}
   }
-
-  /* This works because react-router places the active class on the active NavLink */
-  &:hover,
-  &:active,
-  &.active:any-link {
-    background-color: var(--color-brand-50);
-    color: var(--color-brand-600);
-  }
-
-  & svg {
-    height: 2.4rem;
-    width: 2.4rem;
-    min-width: 2.4rem;
-    color: var(--color-grey-600);
-    transition: color 0.2s;
-  }
-
-  &:hover svg,
-  &:active svg,
-  &.active:any-link svg {
-    color: var(--color-brand-600);
-  }
-
-  & span {
-    opacity: 1;
-    pointer-events: auto;
-    visibility: visible;
-    transition: opacity 0.3s;
-  }
-
-  ${(props) =>
-    props.isCollapsed &&
-    css`
-      &::before,
-      &::after {
-        position: absolute;
-        top: 50%;
-
-        opacity: 0;
-        pointer-events: none;
-        visibility: hidden;
-        transform: translate(-5px, -50%);
-        transition: 0.2s;
-      }
-
-      &::before {
-        content: attr(data-tooltip);
-        background-color: var(--color-grey-800);
-        color: var(--color-brand-50);
-        border-radius: var(--border-radius-sm);
-        font-weight: 500;
-        font-size: 1.4rem;
-        padding: 0.44rem 0.8rem;
-        left: calc(100% + var(--padding-inline) + 5px);
-      }
-
-      &::after {
-        content: "";
-        border: 5px solid transparent;
-        border-right-color: var(--color-grey-800);
-        border-left: none;
-        left: calc(100% + var(--padding-inline) + 1px);
-      }
-
-      &:hover::before,
-      &:hover::after,
-      &:focus::before,
-      &:focus::after {
-        opacity: 1;
-        pointer-events: auto;
-        visibility: visible;
-        transform: translate(0, -50%);
-      }
-
-      & span {
-        opacity: 0;
-        pointer-events: none;
-        visibility: hidden;
-      }
-    `}
 `;
 
 const navLinks = [
@@ -123,17 +125,12 @@ export default function SidebarNav({ isCollapsed }) {
     <nav>
       <NavList>
         {navLinks.map((link) => (
-          <li key={link.name}>
-            <StyledNavLink
-              isCollapsed={isCollapsed}
-              to={link.url}
-              title={link.name}
-              data-tooltip={link.name}
-            >
+          <NavItem key={link.name} isCollapsed={isCollapsed}>
+            <NavLink to={link.url} title={link.name} data-tooltip={link.name}>
               {link.icon}
               <span>{link.name}</span>
-            </StyledNavLink>
-          </li>
+            </NavLink>
+          </NavItem>
         ))}
       </NavList>
     </nav>
