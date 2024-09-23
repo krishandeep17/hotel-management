5a4483902fc7408c80dcdbca8210332f6a31d3bd
krishandeep17 <krishandeep17@gmail.com>
2024-09-23 10:17:07 +0530

    feat: use dynamic import() to code-split the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { lazy, Suspense } from "react";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import { DarkModeProvider } from "./contexts/DarkModeContext";
 import AuthLayout from "./features/authentication/AuthLayout";
-import Account from "./pages/Account";
-import Booking from "./pages/Booking";
-import Bookings from "./pages/Bookings";
-import Cabins from "./pages/Cabins";
-import CheckIn from "./pages/CheckIn";
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
-import PageNotFound from "./pages/PageNotFound";
-import Settings from "./pages/Settings";
-import Users from "./pages/Users";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 import ProtectedRoute from "./ui/ProtectedRoute";
+import Spinner from "./ui/Spinner";
+
+// Dynamic import() to code-split the application
+const Account = lazy(() => import("./pages/Account"));
+const Booking = lazy(() => import("./pages/Booking"));
+const Bookings = lazy(() => import("./pages/Bookings"));
+const Cabins = lazy(() => import("./pages/Cabins"));
+const CheckIn = lazy(() => import("./pages/CheckIn"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Login = lazy(() => import("./pages/Login"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const Settings = lazy(() => import("./pages/Settings"));
+const Users = lazy(() => import("./pages/Users"));
 
 // Create a client
 const queryClient = new QueryClient();
@@ -31,29 +35,31 @@ export default function App() {
 
         <GlobalStyles />
         <BrowserRouter>
-          <Routes>
-            <Route
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Navigate replace to="dashboard" />} />
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="bookings" element={<Bookings />} />
-              <Route path="bookings/:bookingId" element={<Booking />} />
-              <Route path="check-in/:bookingId" element={<CheckIn />} />
-              <Route path="cabins" element={<Cabins />} />
-              <Route path="users" element={<Users />} />
-              <Route path="settings" element={<Settings />} />
-              <Route path="account" element={<Account />} />
-            </Route>
-            <Route element={<AuthLayout />}>
-              <Route path="login" element={<Login />} />
-            </Route>
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+          <Suspense fallback={<Spinner type="fullPage" />}>
+            <Routes>
+              <Route
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="dashboard" />} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="bookings" element={<Bookings />} />
+                <Route path="bookings/:bookingId" element={<Booking />} />
+                <Route path="check-in/:bookingId" element={<CheckIn />} />
+                <Route path="cabins" element={<Cabins />} />
+                <Route path="users" element={<Users />} />
+                <Route path="settings" element={<Settings />} />
+                <Route path="account" element={<Account />} />
+              </Route>
+              <Route element={<AuthLayout />}>
+                <Route path="login" element={<Login />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
 
         <Toaster
